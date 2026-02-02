b6f8090c13cfdcf8ea5557bd1dd54a8211259d76
BHATE OM YOGESH <ombhate1968@gmail.com>
2026-02-02 09:48:00 +0530

    Add portfolio project

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+
+    form.reset();
+});
